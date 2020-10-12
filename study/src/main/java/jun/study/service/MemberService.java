e2cfb164a2e15645d7db2dddeff49ad01a54c69d
khj68 <maga40@naver.com>
2020-10-12 16:29:22 +0900

    Make member service

--- study/src/main/java/jun/study/service/MemberService.java
+++ study/src/main/java/jun/study/service/MemberService.java
@@ -1,12 +1,36 @@
 package jun.study.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import jun.study.domain.Member;
 import jun.study.repository.MemberRepository;
 import jun.study.repository.MemoryMemberRepository;
 
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
+    // 회원가입
     public Long join(Member member) {
-        
+        // 같은 이름 중복 회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }

55bd353ce7858b37c3e06f9716d2e17d2122c890
KHJ <maga40@naver.com>
2020-12-08 17:34:58 +0900

    Create member service test

--- jpashop/src/main/java/jpabook/jpashop/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package jpabook.jpashop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jpabook.jpashop.domain.Member;
-
-public interface UserRepository extends JpaRepository<Member, Long> {
-  
-}

612f18ad164f538aea1adfdd9924de21751019a5
KHJ <maga40@naver.com>
2020-12-10 14:26:30 +0900

    Create order api

---
+++ jpashop/src/main/java/jpabook/jpashop/Hibernate5Module.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop;
+
+import org.springframework.context.annotation.Bean;
+
+public class Hibernate5Module {
+  @Bean
+  Hibernate5Module hibernate5Module() {
+    return new Hibernate5Module();
+  }
+}
--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @EntityScan("jpabook.jpashop.*")
 @SpringBootApplication
@@ -11,5 +14,7 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
+
+	
 
 }
